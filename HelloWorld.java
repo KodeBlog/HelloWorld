ddcd72e86254cfd4d48e8427f9f957d586fb6679
Amanda Kazembe <amanda@kazembe.com>
2015-06-02 09:20:09 +0300

    Added showHello method

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld { 
    public static void main(String[] args) { 
       String name = args[0];
-	  System.out.println("Hi " + name + ", welcome to hello world!");
+	  showHello(name);
+   }
+   
+   private void showHello(String name){
+	   System.out.println("Hi " + name + ", welcome to hello world!");
    }
 }
