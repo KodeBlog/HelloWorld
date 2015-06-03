69ad5c46c5488bd2275acbba95ed018814e18804
Amanda Kazembe <amanda@kazembe.com>
2015-06-03 12:10:44 +0300

    Updated code to display GUI window

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,10 +1,10 @@
 public class HelloWorld { 
    public static void main(String[] args) { 
-      String name = args[0];
-	  showHello(name);
-   }
-   
-   private void showHello(String name){
-	   System.out.println("Hi " + name + ", welcome to hello world!");
+        FORMHelloWorld stForm = new FORMHelloWorld(null, true);
+		FORMHelloWorld.setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+		FORMHelloWorld.pack();
+		FORMHelloWorld.setLocationRelativeTo(null);
+
+		FORMHelloWorld.setVisible(true);
    }
 }
